a5ab249f6613b8e499dbd4223776e3e06f5b5036
vinirrai <vinirrai58@gmail.com>
2023-07-27 17:44:05 -0400

    schedule-removed

--- static/js/main.js
+++ static/js/main.js
@@ -47,66 +47,7 @@ $(document).ready(function () {
         });
     });
 
-    $.getJSON("/static/assets/schedule.json", function (data) {
-
-        var $header = $('<div>');
-        var $tables = $('<div>', { "style": "position: relative;" });
-
-        var i = 0;
-
-        data.forEach(function (date) {
-            $header.append(
-                $('<button>', { "class": (i == 0 ? "day-selector active" : "day-selector"), "id": "day" + i })
-                    .text(date['date'])
-                    .on('click', function () {
-                        $('.day-selector').each(function () {
-                            $(this).removeClass('active');
-                        }) 
-                        $('table').each(function() {
-                            $(this).removeClass('active');
-                        })
-                        $(this).addClass('active');
-                        let id = $(this).attr('id').slice(-1);
-                        $('#table' + id).addClass('active');
-                    })
-            );
-
-            var $table = $('<table>', { "class": (i == 0 ? "active" : ""), "id": "table" + i++ });
-            var $schBody = $('<tbody>');
-
-            $table.append(
-                $('<thead>').append(
-                    $('<tr>')
-                        .append($('<th>', { "style": "width:17.5%; text-align: left;" }).text('Time (Eastern)'))
-                        .append($('<th>', { 'style': "width:25%; text-align: left;" }).text('Type'))
-                        .append($('<th>', { "style": "width:57.5%; text-align: left;" }).text('Event'))
-                        .append($('<th>',{"style": "width:5%; text-align: left;" }).text('Location'))
-                )
-            );
-
-            var j = 0;
-
-            date['schedule'].forEach(function (element) {
-                $schBody.append(
-                    $('<tr>', { "class": (j++ % 2 == 0 ? "table-row-even" : "table-row-odd") })
-                        .append($('<td>').html(element['time']))
-                        .append($('<td>').html(element['type']))
-                        .append($('<td>').html(element['event']))
-                        .append($('<td>').html(element['location']))
-                )
-            });
-
-            $table.append($schBody);
-            $tables.append($table);
-        });
-
-        $("#schedule-container").append($header);
-        $tables.append(
-            $('<img>', { "class": "sch-img hide-on-med-and-down", "src": "/static/assets/images/art/kraken.svg" })
-        );
-        $("#schedule-container").append($tables);
-    });
-
+    
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
